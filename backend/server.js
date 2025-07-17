8aff185ddf6b166e6477c8e5a9884871e1dac8ff
Brendan Hendrata <bhendrata@cmu.edu>
2025-07-17 19:36:45 -0400

    updated PORT to environment variable

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,13 @@ import productRoutes from './routes/product.routes.js';
 
 dotenv.config()
 const app = express();
+const PORT = process.env.port || 5000;
 
 app.use(express.json()); // allows to accept JSON in body (req.body) // type of middleware 
 
 app.use("/api/products", productRoutes); // 1 for each route
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:" + PORT);
 });
