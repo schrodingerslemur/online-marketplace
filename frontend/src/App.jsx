93253205431975e5529fa50e55087612438cc8c5
Brendan Hendrata <bhendrata@cmu.edu>
2025-07-17 22:42:00 -0400

    frontend framework setup with vite, react and chakra ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@chakra-ui/react";
+
+const App = () => {
+  console.log("App is rendering!");
+
+  return (
+    <Button>
+      Test
+    </Button>
+  );
+};
+
+export default App;
